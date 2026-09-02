5215a4eb7f13a1be5e09312d54c43070d6b27c78
Alexander <alexander.mejinski@gmail.com>
2026-09-02 23:51:42 +0300

    test: add invite code format and duplicate uniqueness tests (issue 6)

--- tests/test_household.py
+++ tests/test_household.py
@@ -1,4 +1,7 @@
+import re
+
 from django.core.exceptions import ValidationError
+from django.db import IntegrityError
 from django.test import TestCase
 
 from households.models import Household
@@ -28,6 +31,18 @@ class HouseholdModelTest(TestCase):
     def test_invite_code_auto_generated_and_unique(self):
         h1 = Household.objects.create(name="A")
         h2 = Household.objects.create(name="B")
+        h3 = Household.objects.create(name="C")
         self.assertTrue(h1.invite_code)
         self.assertTrue(h2.invite_code)
-        self.assertNotEqual(h1.invite_code, h2.invite_code)
+        self.assertTrue(h3.invite_code)
+        pattern = re.compile(r"^HOME-[A-Z0-9]{4}$")
+        for h in (h1, h2, h3):
+            self.assertRegex(h.invite_code, pattern)
+        codes = {h1.invite_code, h2.invite_code, h3.invite_code}
+        self.assertEqual(len(codes), 3)
+
+    def test_duplicate_invite_code_rejected(self):
+        h1 = Household.objects.create(name="X")
+        duplicate = Household(name="Y", invite_code=h1.invite_code)
+        with self.assertRaises(IntegrityError):
+            duplicate.save()
