08e9704895e68d703e880c4c3cf2259d20127018
Alexander <alexander.mejinski@gmail.com>
2026-09-03 19:54:23 +0300

    feat(app): render home page template with role-based content

--- app/views.py
+++ app/views.py
@@ -1,5 +1,14 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from households.models import HouseholdMember
 
 
 def home(request):
-    return HttpResponse("Zoomcamp AI Chores")
+    context = {}
+    if request.user.is_authenticated:
+        membership = HouseholdMember.objects.select_related("household").filter(
+            user=request.user
+        ).first()
+        if membership:
+            context["household_name"] = membership.household.name
+    return render(request, "app/home.html", context)
