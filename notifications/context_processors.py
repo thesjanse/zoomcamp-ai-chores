202783a9bdfc15995e7375ebf9120c3e5c449456
Alexander <alexander.mejinski@gmail.com>
2026-09-03 19:26:58 +0300

    feat: add Bootstrap navbar to base template (#20)

--- notifications/context_processors.py
+++ notifications/context_processors.py
@@ -1,9 +1,16 @@
+from households.models import HouseholdMember
 from notifications.models import Notification
 
 
 def notifications(request):
     if not request.user.is_authenticated:
-        return {"unread_notifications": [], "unread_count": 0}
+        return {
+            "unread_notifications": [],
+            "unread_count": 0,
+            "has_household": False,
+            "is_admin": False,
+        }
+    membership = HouseholdMember.objects.filter(user=request.user).first()
     unread = (
         Notification.objects.filter(recipient=request.user, is_read=False)
         .order_by("-created_at")[:5]
@@ -11,4 +18,6 @@ def notifications(request):
     return {
         "unread_notifications": unread,
         "unread_count": unread.count(),
+        "has_household": membership is not None,
+        "is_admin": membership.is_admin if membership else False,
     }
